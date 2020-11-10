0005de7bb5f03c76f30b8e72da15cda9906ab07c
josemagr95 <josegomezrivera95@gmail.com>
2020-11-09 21:43:51 -0300

    click on elements

--- js/script.js
+++ js/script.js
@@ -32,22 +32,26 @@ $( ".tsunamis" ).click(function() {
 $( ".click-valparaiso" ).click(function() {    
     $( "#talcahuano, #cartagena, #iquique").hide();
     $( "#valparaiso" ).show();
+    pauseVideosAudios("video-valparaiso");    
 });
 
 $( ".click-cartagena" ).click(function() {
     $( "#talcahuano, #valparaiso, #iquique").hide();
-    $( "#cartagena" ).show();
+    $( "#cartagena" ).show();    
+    pauseVideosAudios("video-cartagena");   
 });
 
 
 $( ".click-talcahuano" ).click(function() {
     $( "#valparaiso, #cartagena, #iquique").hide();
     $( "#talcahuano" ).show();
+    pauseVideosAudios("video-talcahuano");   
 });
 
 $( ".click-iquique" ).click(function() {
     $( "#talcahuano, #cartagena, #valparaiso").hide();
     $( "#iquique" ).show();
+    pauseVideosAudios("video-iquique");   
 });
 
 $( "#canvas" ).click(function() {
@@ -58,21 +62,33 @@ $("#canvas").on("tap",function(){
     $(".container-icono-3d").hide(1500);
 });
 
-document.addEventListener('play', function(e){
-    var audios = document.getElementsByTagName('audio');
-    var videos = document.getElementsByClassName('video-hablando');
-    for(var i = 0, len = audios.length; i < len;i++){
-        if(audios[i] != e.target){
-            audios[i].pause();
-            audios[i].currentTime = 0;
+function pauseVideosAudios(multimedia) {
+    var video = document.getElementById(multimedia);
+    video.play();
+}
+
+
+
+$(document).ready(function(){
+    document.addEventListener('play', function(e){      
+        var audios = document.getElementsByTagName('audio');
+        var videos = document.getElementsByClassName('video-hablando');
+        for(var i = 0, len = audios.length; i < len;i++){
+            if(audios[i] != e.target){
+                audios[i].pause();
+                audios[i].currentTime = 0;
+            }
         }
-    }
-    for(var i = 0, len = videos.length; i < len;i++){
-        if(videos[i] != e.target){
-            videos[i].pause();
-            videos[i].currentTime = 0;
+        for(var i = 0, len = videos.length; i < len;i++){
+            if(videos[i] != e.target){
+                videos[i].pause();
+                videos[i].currentTime = 0;
+            }
         }
-    }
-}, true);
+        
+    }, true);
+      });
+
+
 
 
